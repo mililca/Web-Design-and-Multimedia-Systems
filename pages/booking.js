c38520a8948937eb3c7fb2acffce55dfdb7bf968
mililca <141839434+mililca@users.noreply.github.com>
2024-03-17 20:17:37 +0100

    Add form validation on booking page

--- pages/booking.js
+++ pages/booking.js
@@ -10,3 +10,63 @@ window.onload = function(){
     var cabinClass = localStorage.getItem('cabinClass');
     document.getElementById("cabin-class").innerHTML = `${cabinClass}`;
 }
+
+function checkFirstName() {
+    let fName = document.forms["myFormOne"]["fname"].value;
+    let regName = /^[A-Z].*/;
+    if(fName == "" || !fName.match(regName)){
+        alert("Please enter valid first name");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function checkLastName() {
+    let lName = document.forms["myFormOne"]["lname"].value;
+    let regName = /^[A-Z].*/;
+    if(lName == "" || !lName.match(regName)){
+        alert("Please enter valid last name");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function checkEmail() {
+    let email = document.forms["myFormTwo"]["email"].value;
+    let regName = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if(email == "" || !email.match(regName)){
+        alert("Please enter valid email");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function checkPassport() {
+    let passport = document.forms["myFormOne"]["passport"].value;
+    if(passport == "" || passport.length < 8){
+        alert("Please enter valid passport number");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function checkPhone() {
+    let phone = document.forms["myFormTwo"]["phone"].value;
+    let regName = /^\+.*/;
+    if(phone == "" || !phone.match(regName) || phone.length < 8){
+        alert("Please enter valid phone number");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validateData(){
+    if(checkFirstName() == true && checkLastName() == true && checkPassport() == true && checkPhone() == true && checkEmail() == true){
+        alert("Login Successful");
+    }
+}
